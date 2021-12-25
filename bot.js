4bc479e7869b9772ae8053dff5b9e765bfbb0f65
SPlRlT SER <96570722+SPlRlT-YT@users.noreply.github.com>
2021-12-25 11:56:55 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -139,7 +139,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
         });
 
         console.log(
-            chalk.green.bold('✅ RDX BOT working😈!')
+            chalk.green.bold('✅ SPlRlT BOT BOT working😈!')
         );
         await new Promise(r => setTimeout(r, 1100));
 
@@ -383,8 +383,8 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
                                     'Gerçekleşen Hata: ' + error + '\n\n'
                                     , MessageType.text);
                             } else {
-                                await conn.sendMessage(conn.user.jid, '*~_________~ *OXYMORON ERROR FOUND!* ~______~*' +
-                                    '\n\n*🧞‍♂️ ' + error + '*\n\n```*error connect owner https://wa.me/qr/62FORLJKJZ4WP1 and massage me im coming your oxymoron bot``` ' 
+                                await conn.sendMessage(conn.user.jid, '*~_________~ *SPlRlT BOT ERROR FOUND!* ~______~*' +
+                                    '\n\n*🧞‍♂️ ' + error + '*\n\n```*error connect owner https://wa.me/qr/62FORLJKJZ4WP1 and massage me im coming your SPlRlT BOT bot``` ' 
                                     , MessageType.text);
                             }
                         }
